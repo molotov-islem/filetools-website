237256c1aa9e620768af4159324d21909dcd4e1c
molotov-islem <islam.laib@etu.univ-batna2.dz>
2025-08-30 17:17:34 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
